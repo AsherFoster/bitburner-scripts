0988688075b75af63dfeec475ace8b731ba9534b
Asher Foster <me@asherfoster.com>
2022-01-24 01:02:11 +1300

    deleted some code

--- src/entries/event-listen.ts
+++
@@ -1,6 +0,0 @@
-import type {NS} from '../NetscriptDefinitions';
-import eventBus from '../eventBus';
-
-export async function main(ns: NS): Promise<void> {
-  ns.tprint(await eventBus.next('test'));
-}
--- src/graveyard/spawn.ts
+++
@@ -1,6 +0,0 @@
-import type {NS} from '../NetscriptDefinitions';
-
-// In exec-heavy scripts, exec this to avoid memory constraints
-export async function main(ns: NS) {
-  ns.spawn(ns.args[0].toString());
-}
